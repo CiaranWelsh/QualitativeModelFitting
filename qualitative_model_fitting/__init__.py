98977d6c9baaa9f1b885e2a388121717bdab01fa
CiaranWelsh <ciaran.welsh@newcastle.ac.uk>
2019-08-28 23:17:31 +0100

    finished some todo items

--- qualitative_model_fitting/__init__.py
+++ qualitative_model_fitting/__init__.py
@@ -1,15 +1,16 @@
 """
 todo write a docstring
 todo all documentation
-todo import necessary in __init__
-todo clean up the rest of the repo
 todo expand grammer to allow expressions
 
 """
 import logging
 
-# from qualitative_model_fitting._api import manual_interface
-# from qualitative_model_fitting._simulator import TimeSeries, TimeSeriesPlotter
+from qualitative_model_fitting._api import manual_interface
+from qualitative_model_fitting._simulator import TimeSeries, TimeSeriesPlotter
+from qualitative_model_fitting._parser import Parser
+from qualitative_model_fitting._runner import ManualRunner
+from qualitative_model_fitting._interpreter import Interpreter
 
 LOGGING_LEVEL = logging.DEBUG
 
