46831a08bb16e9a4f012af5b62a9485de4908e41
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2019-09-20 14:41:17 +0100

    draft 1 finished of Dash app

--- web_interface/extended_pi3k_with_dash/app.py
+++ web_interface/extended_pi3k_with_dash/app.py
@@ -43,14 +43,6 @@ css = {
 
 }
 
-# Dash app begin
-app = dash.Dash(
-    __name__,
-    assets_folder=ASSETS_DIR,
-    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
-
-)
-
 # inputs string
 model_inputs = [
     'Insulin',
@@ -72,118 +64,7 @@ with open(PLOTTABLE_SPECIES_FILE, 'r') as f:
     species = f.read().split('\n')
 species = sorted([i.strip() for i in species])
 
-app.layout = html.Div(children=[
-    html.H1(children='Extended PI3K Model'),
-
-    # for graph and inputs
-    html.Div([
-
-        # a left panel for inputs
-        html.H2('Select Model Inputs'),
-        html.Div([
-            dcc.Checklist(
-                id='model_inputs',
-                options=[
-                    {'label': i, 'value': i} for i in model_inputs
-                ],
-                # multi=True,
-                value=['Insulin', 'AA'],
-                labelStyle={"display": "inline-block"},
-                style={'text-align': 'center'}
-            ),
-
-            html.H2('Select Model Outputs'),
-            dcc.Dropdown(
-                id='outputs',
-                options=[{'label': i, 'value': i} for i in species],
-                value=['pmTORC1'],
-                multi=True
-            ),
-            html.Button(id='all_output_btn', children='All Outputs'),
-            html.Button(id='all_pi3k_output_btn', children='PI3K Outputs'),
-            html.Button(id='active_pi3k_output_btn', children='Active PI3K Outputs'),
-            html.Button(id='erk_output_btn', children='Erk Output'),
-            html.Button(id='ampk_output_btn', children='AMPK Output'),
-            html.Button(id='ip3_output_btn', children='IP3 Output'),
-            html.Button(id='e2_output_btn', children='Estrogen Output'),
-            html.Button(id='phenom_output_btn', children='Phenomenological Output'),
-            html.Button(id='infg_output_btn', children='INFg Output'),
-            html.Button(id='trp_output_btn', children='Trp Output'),
-
-            html.H2('Integration Parameters'),
-            html.Form([
-                html.Label('Start'),
-                dcc.Input(placeholder='', type='text', value=0, id='start', style={'float': 'left'}),
-                html.Label('Stop'),
-                dcc.Input(placeholder='', type='text', value=100, id='stop', style={'float': 'left'}),
-                html.Label('Step'),
-                dcc.Input(placeholder='', type='text', value=101, id='step', style={'float': 'left'}),
-            ],
-                style={'display': 'flex'},
-                # className='form-inline'
-            )
-
-        ]),
-
-        # right panel is graph
-        html.Div([dcc.Graph(id='graph')]),
-    ]),
-
-    # for network image
-    html.Div([
-        DashCanvas(id='canvas_image',
-                   image_content=img,
-                   tool='select',
-                   )
-    ]),
-
-    html.Div([
-        dcc.Markdown(model_string, id='model_string')
-    ])
-
-], style={
-    # 'background-color': 'coral',
-    'width': '100%',
-    'height': '100%'
-})
-
-
-@app.callback(
-    Output(component_id='graph', component_property='figure'),
-    [
-        Input(component_id='model_string', component_property='children'),
-        Input(component_id='model_inputs', component_property='value'),
-        Input(component_id='start', component_property='value'),
-        Input(component_id='stop', component_property='value'),
-        Input(component_id='step', component_property='value'),
-        Input(component_id='outputs', component_property='value'),
-    ]
-)
-def update_inputs(model_string, model_inputs, start, stop, step, outputs):
-    model_inputs = {i: 1 for i in model_inputs}
-    ts = TimeSeries(model_string, model_inputs, float(start), float(stop), int(step)).simulate()
-    ts = ts[outputs]
-    traces = []
-    for i in ts:
-        traces.append(
-            go.Scatter(
-                x=np.array(ts.index),
-                y=ts[i],
-                name=i,
-                mode='lines'
-            )
-        )
-
-    return {
-        'data': traces,
-        'layout': go.Layout(
-            xaxis={'title': 'Time (AU)'},
-            yaxis={'title': '[AU]'},
-            showlegend=True
-        )
-    }
-
-
+# dictionary to support the output buttons callback
 NCLICKS_DCT = OrderedDict(
     all_output_btn={
         'n_clicks': 0,
@@ -231,7 +112,7 @@ NCLICKS_DCT = OrderedDict(
         'n_clicks': 0,
         'output': ['E2_cyt', 'ERa_cyt', 'ERa_E2', 'ERa_dimer',
                    'ERa_dimer_nuc', 'ERa_nuc', 'TFFmRNA', 'Greb1mRNA', 'TFF',
-                   'Greb1', 'mER'
+                   'Greb1',
                    ]
     },
     phenom_output_btn={
@@ -253,12 +134,211 @@ NCLICKS_DCT = OrderedDict(
 )
 
 
+# external functions
+
+def plot_graph(model_string, model_inputs, start, stop, step, outputs):
+    """
+    Plot the graph
+
+    Args:
+        model_string:
+        model_inputs:
+        start:
+        stop:
+        step:
+        outputs:
+
+    Returns:
+
+    """
+    model_inputs = {i: 1 for i in model_inputs}
+    ts = TimeSeries(model_string, model_inputs, float(start), float(stop), int(step)).simulate()
+    ts = ts[outputs]
+    traces = []
+    for i in ts:
+        traces.append(
+            go.Scatter(
+                x=np.array(ts.index),
+                y=ts[i],
+                name=i,
+                mode='lines'
+            )
+        )
+
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            xaxis={'title': 'Time (AU)'},
+            yaxis={'title': '[AU]'},
+            showlegend=True
+        )
+    }
+
+
+# Dash app begin
+app = dash.Dash(
+    __name__,
+    assets_folder=ASSETS_DIR,
+    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+
+)
+
+app.layout = html.Div(children=[
+    html.H1(children='Extended PI3K Model'),
+
+    # for graph and inputs
+    html.Div([
+
+        # a left panel for inputs
+        html.H2('Select Model Inputs'),
+        html.Div([
+            dcc.Checklist(
+                id='model_inputs',
+                options=[
+                    {'label': i, 'value': i} for i in model_inputs
+                ],
+                # multi=True,
+                value=['Insulin', 'AA'],
+                labelStyle={"display": "inline-block"},
+                style={'text-align': 'center'}
+            ),
+
+            html.H2('Select Model Outputs'),
+            dcc.Dropdown(
+                id='outputs',
+                options=[{'label': i, 'value': i} for i in species],
+                value=NCLICKS_DCT['active_pi3k_output_btn']['output'],
+                multi=True
+            ),
+            html.Button(id='all_output_btn', children='All Outputs'),
+            html.Button(id='all_pi3k_output_btn', children='PI3K Outputs'),
+            html.Button(id='active_pi3k_output_btn', children='Active PI3K Outputs'),
+            html.Button(id='erk_output_btn', children='Erk Output'),
+            html.Button(id='ampk_output_btn', children='AMPK Output'),
+            html.Button(id='ip3_output_btn', children='IP3 Output'),
+            html.Button(id='e2_output_btn', children='Estrogen Output'),
+            html.Button(id='phenom_output_btn', children='Phenomenological Output'),
+            html.Button(id='infg_output_btn', children='INFg Output'),
+            html.Button(id='trp_output_btn', children='Trp Output'),
+
+            html.H2('Integration Parameters'),
+            html.Form([
+                html.Label('Start'),
+                dcc.Input(placeholder='', type='text', value=0, id='start', style={'float': 'left'}),
+                html.Label('Stop'),
+                dcc.Input(placeholder='', type='text', value=100, id='stop', style={'float': 'left'}),
+                html.Label('Step'),
+                dcc.Input(placeholder='', type='text', value=101, id='step', style={'float': 'left'}),
+            ],
+                style={'display': 'flex'},
+                # className='form-inline'
+            )
+
+        ]),
+
+        # right panel is graph
+        html.Div([dcc.Graph(
+            id='graph', figure=plot_graph(
+                model_string,
+                {i: 1 for i in ['Insulin', 'AA']},
+                0, 100, 101,
+                NCLICKS_DCT['active_pi3k_output_btn']['output'],
+            )
+        )
+        ]),
+    ]),
+
+    # for network image
+    html.Div([
+        html.Img(
+            src='/assets/network3.png',
+            style={
+                'width': '100%',
+                'height': '100%',
+            }
+        ),
+        # DashCanvas(id='canvas_image',
+        #            image_content=img,
+        #            tool='select',
+        #            zoom=10,
+        #            scale=2,
+        #            height=2000,
+        #            ),
+        # html.Label('Width Slider'),
+        # dcc.Slider(
+        #     id='width_slider',
+        #     min=100,
+        #     max=2000,
+        #     step=20,
+        #     value=800
+        # ),
+        # html.Label('Height Slider'),
+        # dcc.Slider(
+        #     id='height_slider',
+        #     min=100,
+        #     max=2000,
+        #     step=20,
+        #     value=800
+        # ),
+    ], style={
+        'height': '100%',
+        'width': '100%'
+    }),
+
+    html.Div([
+        dcc.Textarea(
+            id='model_string',
+            value=model_string,
+            style={
+                'width': '100%',
+                'height': '100%',
+            }
+        ),
+    ],
+        style={'height': '1000px'})
+
+], style={
+    # 'background-color': 'coral',
+    'width': '100%',
+    'height': '100%'
+})
+
+
+@app.callback(
+    Output(component_id='graph', component_property='figure'),
+    [
+        Input(component_id='model_string', component_property='value'),
+        Input(component_id='model_inputs', component_property='value'),
+        Input(component_id='start', component_property='value'),
+        Input(component_id='stop', component_property='value'),
+        Input(component_id='step', component_property='value'),
+        Input(component_id='outputs', component_property='value'),
+    ]
+)
+def update_inputs_callback(model_string, model_inputs, start, stop, step, outputs):
+    """
+    update graph
+    Args:
+        model_string:
+        model_inputs:
+        start:
+        stop:
+        step:
+        outputs:
+
+    Returns:
+
+    """
+    return plot_graph(model_string, model_inputs, start, stop, step, outputs)
+
+
 @app.callback(
     Output('outputs', 'value'),
     [Input(i, 'n_clicks') for i in list(NCLICKS_DCT.keys())]
 )
 def output_callbacks(*args):
-    print(args)
+    # default return
+    to_return = NCLICKS_DCT['active_pi3k_output_btn']['output']
     if all(args) is None:
         raise dash.exceptions.PreventUpdate
 
@@ -266,17 +346,37 @@ def output_callbacks(*args):
         btn = list(NCLICKS_DCT.keys())[i]
         nclick = args[i]
         existing_nclick = NCLICKS_DCT[btn]['n_clicks']
-        print('btn', btn)
-        print('nclick', nclick)
-        print('existing_nclick', existing_nclick)
 
         if nclick is not None:
             if nclick != existing_nclick:
-                print('doing a return')
                 NCLICKS_DCT[btn]['n_clicks'] = nclick
-                return NCLICKS_DCT[btn]['output']
+                to_return = NCLICKS_DCT[btn]['output']
             else:
                 continue
+    print(to_return)
+    return to_return
+
+
+# Here are the available properties in "width_slider":
+['id', 'marks', 'value', 'className', 'disabled', 'dots',
+ 'included', 'min', 'max', 'tooltip', 'step', 'vertical',
+ 'updatemode', 'loading_state', 'persistence', 'persisted_props',
+ 'persistence_type']
+
+
+# @app.callback(
+#     Output('canvas_image', 'width'),
+#     [Input('width_slider', 'value')]
+# )
+# def update_image_width(value):
+#     return value
+#
+# @app.callback(
+#     Output('canvas_image', 'height'),
+#     [Input('height_slider', 'value')]
+# )
+# def update_image_width(value):
+#     return value
 
 
 if __name__ == '__main__':
