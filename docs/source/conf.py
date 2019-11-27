8dd13afa6937e466cf3b503db20ceb35472455d0
CiaranWelsh <ciaran.welsh@newcastle.ac.uk>
2019-11-27 17:36:11 +0000

    modify docs conf to include index as root

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+master_doc = 'index.rst'
 
 # -- Options for HTML output -------------------------------------------------
 
